9a10a0817dc9f6bfa731ae54d913b53bb2da13e2
Anderson Manly <17306168+MandersonMuahahaha@users.noreply.github.com>
2018-08-30 09:49:09 -0500

    Adding information pulled from AWC

--- airport_wx/lib/AirportWx/Controller/Icao.pm
+++ airport_wx/lib/AirportWx/Controller/Icao.pm
@@ -3,32 +3,42 @@ use Mojo::Base 'Mojolicious::Controller';
 use Mojo::UserAgent;
 use Mojo::URL;
 use Mojo::JSON 'decode_json';
+use Mojo::DOM;
 
 sub airport {
   my $self = shift;
 
-  # Render template "icao/airport.html.ep" with message
-  $self->render(msg => 'Welcome to the ICAO airport code weather lookup!');
+  # Render template "icao/airport.html.ep"
+  $self->render();
 }
 
 sub getWx {
   my $self = shift;
   my $ua = Mojo::UserAgent->new;
-  my $url = 'https://soa.smext.faa.gov/asws/api/airport/status/';
 
   # Get the airport code the user submitted
   my $ac = lc $self->param('airportCode');
   # Strip off any leading "K"
   $ac =~ s/^[kK](\w{3})/$1/;
-  # Combine the two
-  $url = $url . $ac;
+
+  my $FAAurl = 'https://soa.smext.faa.gov/asws/api/airport/status/';
+  my $AWCurl = Mojo::URL->new('https://aviationweather.gov/adds/dataserver_current/httpparam')->query(
+    dataSource => 'metars',
+    requestType => 'retrieve',
+    format => 'xml',
+    hoursBeforeNow => 1,
+    mostRecent => 'true',
+    stationString => "K" . uc($ac)
+  );
 
   #Execute the query and return a decoded JSON object
-  my $decodedjson = decode_json ($ua->get($url)->result->body);
+  my $decodedjson = decode_json ($ua->get($FAAurl . $ac)->result->body);
+  my $AWCdom = Mojo::DOM->new($ua->get($AWCurl)->result->body);
 
   $self->render(
     template => 'icao/results',
     decodedjson => $decodedjson,
+    AWCdom => $AWCdom
   );
 
 }
